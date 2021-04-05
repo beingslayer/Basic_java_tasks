2281fefa0d015d353394a8ddaca23d3f317c014f
beingslayer <60589027+beingslayer@users.noreply.github.com>
2021-04-05 10:39:29 +0530

    Update FlightDetails.java

--- day4_airlines/FlightDetails.java
+++ day4_airlines/FlightDetails.java
@@ -1,39 +1,31 @@
-package  day4_airlines;
-
-
+package day4_airlines;
 
 public class FlightDetails {
 
-	public void printFlightDetails(Flight f)
-	{
-		/* Write code to print the flight information and fare break up*/
-		
-		System.out.printf("Flight Number-->"+f.getAirliner());
-		
-		System.out.printf("\nFlight Name-->"+f.getFlightNumber());
-		
-		System.out.printf("\nSource-->"+f.getSource());
-		
-		System.out.printf("\nFlight Fly Date time-->"+f.getFlyDateTime());
-		
-		
-		System.out.printf("\nDestination-->"+f.getDestination());
-		
-		System.out.printf("\nArrival date and time-->"+f.getArrivalDateTime());
-		if(TravelApp.hault_no>0)
-		System.out.println("\nHaults:-");
-		
-		for(Hault ht:f.getHault()) {
-			System.out.println("\t\tAirport: "+ht.getAirportName());
-			System.out.println("\t\tDuration:"+ht.getDuration());
-		}
-		System.out.printf("\nTax Rate-->"+f.getInternationFlyTax());
-		System.out.println("\nBaseFare-->"+f.getBaseFare());
+	public void printFlightDetails(Flight f) {
+		/* Write code to print the flight information and fare break up */
+
+		System.out.printf("Flight Number-->" + f.getAirliner());
+		System.out.printf("\nFlight Name-->" + f.getFlightNumber());
+		System.out.printf("\nSource-->" + f.getSource());
+		System.out.printf("\nFlight Fly Date time-->" + f.getFlyDateTime());
+		System.out.printf("\nDestination-->" + f.getDestination());
+		System.out.printf("\nArrival date and time-->" + f.getArrivalDateTime());
+		if (TravelApp.hault_no > 0) {
+			System.out.println("\nHaults:-");
+		for (Hault ht : f.getHault()) {
+			System.out.println("\t\tAirport: " + ht.getAirportName());
+			System.out.println("\t\tDuration:" + ht.getDuration());
+		}}
+		System.out.printf("\nTax Rate-->" + f.getInternationFlyTax());
+		System.out.println("\nBaseFare-->" + f.getBaseFare());
 		System.out.println("\t****Fare Breakup******\t");
-		System.out.println("\tAirport Surcharge--->"+f.getAirport_surcharge());
-		System.out.println("\tFuel Charge---->"+f.getFuel_cost());
-		System.out.println("\t********");
-		System.out.println("\nFinal Cost-->"+f.getFinalCost());
-			
+		System.out.println("\tAirport Surcharge--->" + f.getAirport_surcharge());
+		System.out.println("\tFuel Charge---->" + f.getFuel_cost());
+		System.out.println("\tHoliday Fee----->"+f.getHolidayfee());
+		System.out.println("\tHours Fee---->"+f.getHoursfee());
+		System.out.println("\tNon-Friendly fee---->"+f.getNonfriendlytax());
+		System.out.println("\t*************************");
+		System.out.println("\nFinal Cost-->" + f.getFinalCost());
 	}
 }
