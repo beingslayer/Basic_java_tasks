895142dcb81ca8a490ba390413cd33e575c57f69
Being Slayer <60589027+beingslayer@users.noreply.github.com>
2021-05-14 21:50:45 +0530

    Update CopyDataThread.java

--- lab8_assignment/lab8/lab8/CopyDataThread.java
+++ lab8_assignment/lab8/lab8/CopyDataThread.java
@@ -3,21 +3,24 @@ import java.io.*;
 
 public class CopyDataThread extends Thread {
 	
-    public CopyDataThread(FileWriter f1, File f2) throws IOException, InterruptedException {
+    public CopyDataThread(File f1, File f2) throws IOException, InterruptedException {
     	
     	FileReader fr=new FileReader("f1"); 
     	int x=fr.read( );
         FileWriter f = new FileWriter(f2); 
         int i = 0;
+        
     	while( x!=-1)
     	 {  f.write((char)x);
     	    i++;
     	    if(i%10 == 0) {
     	       System.out.println("10 characters are copied");
-    	       sleep(500);
+    	     
+    	   
     	    }
-    	 x=fr.read();
     	 
+    	    x=fr.read();
     	 }
+    	  f.close();
 	}
 }
